daa659ac7caac6ea20580965cfffa03105cc6ecd
Miray <mirayekerr@gmail.com>
2021-05-28 17:30:20 +0300

    Dictionary data type

---
+++ Dictionary_examples/main.py
@@ -0,0 +1,24 @@
+Cv1={"Name:": "Mustafa Kemal","Surname:":"ATATURK","City:":"Turkey","Occupation:":"Founder"}
+Cv2={"Name:": "Emre","Surname:":"BOLAT","City:":"USA","Occupation:":"Engineer"}
+Cv3={"Name:": "Hannah","Surname:":"MOSKORA","City:":"Sweden","Occupation:":"Data Science"}
+Cv4={"Name:": "Miray","Surname:":"EKER","City:":"Canada","Occupation:":"Pythoner"}
+Cv5={"Name:": "Katty","Surname:":"PERRY","City:":"Switzerland","Occupation:":"Machine Learning"}
+print("CV1 Bilgiler")
+for i,j in Cv1.items():
+    print(i,j)
+print("***********************************")
+print("CV2 Bilgiler")
+for i,j in Cv2.items():
+    print(i,j)
+print("***********************************")
+print("CV3 Bilgiler")
+for i,j in Cv3.items():
+    print(i,j)
+print("***********************************")
+print("CV4 Bilgiler")
+for i,j in Cv4.items():
+    print(i,j)
+print("***********************************")
+print("CV5 Bilgiler")
+for i,j in Cv5.items():
+    print(i,j)
